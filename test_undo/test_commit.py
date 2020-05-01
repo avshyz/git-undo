bac6d4f3542ec5d8f87d64699dfa3d27740a3f5f
Ehud <ehud@nymhealth.com>
2020-05-01 18:37:27 +0300

    formatting

--- test_undo/test_commit.py
+++ test_undo/test_commit.py
@@ -10,8 +10,9 @@ def test_undo_strategy_undoes_commit(repo: git.Repo):
     empty_file = add_empty_file(repo, 'commit_strategy_undo')
     head_before_commit = repo.head
     repo.index.commit('commit to revert')
-    Commit.undo('git commit', repo)
     
+    Commit.undo('git commit', repo)
+
     assert repo.head == head_before_commit
     assert empty_file.name in {name for name, stage in repo.index.entries}
     assert os.path.isfile(empty_file)

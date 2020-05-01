51eb3a0663997f5877a03e9f5e939830526ff070
Ehud <ehud@nymhealth.com>
2020-05-01 18:49:48 +0300

    formatting

--- test_undo/test_checkout.py
+++ test_undo/test_checkout.py
@@ -1,5 +1,3 @@
-import os
-
 import git
 
 from undo.strategies import Checkout
@@ -8,10 +6,9 @@ from undo.strategies import Checkout
 def test_undo_strategy_undoes_checkout(repo: git.Repo):
     head_before_commit = repo.head
 
-    repo.create_head("branch2")
-    assert repo.head == head_before_commit
-    repo.heads.branch2.checkout()
+    branch2 = repo.create_head("branch2")
+    branch2.checkout()
 
     Checkout.undo('git checkout', repo)
-    
+
     assert repo.head == head_before_commit

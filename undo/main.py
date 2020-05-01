37433c5772db95e040d44ee2c5795e78e62474d7
Avishay Zarad <zarad.avishay@gmail.com>
2020-05-01 17:39:54 +0300

    Adding get_latest_git_command

--- undo/main.py
+++ undo/main.py
@@ -1,2 +1,15 @@
+import os
+import re
+
+
+def get_latest_git_command():
+    with open('~/.zsh_history') as history_file:
+        for history_line in history_file.readlines()[::-1]:
+            # TODO add aliases expansion
+            if "git" in history_line:
+                # in cause of multiple commands (with ; or &), will look for the last git command
+                return re.findall("git[^;&]+(?=[;&])|git.+$", history_line)[-1]
+
+
 def main():
     print('hello world!')
